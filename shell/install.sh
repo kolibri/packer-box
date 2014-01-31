1871ec74682118ca656ed56abed9f1adc3bc0568
Lukas Sadzik <lukas.sadzik@sensiolabs.de>
2014-01-31 17:56:11 +0100

    Enabled nfs support for vagrant

--- shell/install.sh
+++ shell/install.sh
@@ -22,6 +22,9 @@ cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=vagrant' /etc/sudoers
 sed -i -e 's/%vagrant ALL=(ALL) ALL/%vagrant ALL=NOPASSWD:ALL/g' /etc/sudoers
 
+# Install necessary libraries for guest additions and Vagrant NFS Share
+sudo apt-get -y -q install linux-headers-$(uname -r) build-essential dkms nfs-common
+
 # install puppet3
 #wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
 #dpkg -i puppetlabs-release-precise.deb
